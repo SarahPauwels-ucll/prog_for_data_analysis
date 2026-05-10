ed09fa72e9887e698b548f7475eb5a6d17cf8388
SarahPauwels-ucll <r0875579@ucll.be>
2026-05-10 14:53:24 +0100

    4 remove PM10 from model in application

--- pages/model.py
+++ pages/model.py
@@ -24,7 +24,7 @@ if df is not None:
     stations = sorted(df['station'].unique())
     selected_station = st.sidebar.selectbox("Select a Station", stations)
 
-    polutants = ["PM2.5","PM10","SO2","NO2","CO", "O3"]
+    polutants = ["PM2.5","SO2","NO2","CO", "O3"]
     selected_polutant = st.sidebar.radio(
         "Select Polutant to Display",
         options=polutants
